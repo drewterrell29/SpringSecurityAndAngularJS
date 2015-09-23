06f0eacea430dc57ce9c70d3f5e648b497424da0
drewterrell29 <drewterrell29@gmail.com>
2015-09-23 06:17:09 -0500

    Commiting code so far
    Commiting code so far

---
+++ src/main/java/com/experiment/SpringSecurityAndAngularJsApplication.java
@@ -0,0 +1,12 @@
+package com.experiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAndAngularJsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityAndAngularJsApplication.class, args);
+    }
+}
